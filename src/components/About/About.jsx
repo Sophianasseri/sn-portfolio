0c0cdf5f768a0df7a364f5ba45fdb826c9506677
Sophia Nasseri <nasseri.sophia@gmail.com>
2022-09-07 17:39:36 +0200

    feat: responsive homepage

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import flower from '../../assets/flower.svg';
+import flowerMobile from '../../assets/flowermobile.svg';
 import styles from './about.module.css';
 
 function About() {
@@ -10,7 +11,7 @@ function About() {
         <p>
           Après une licence de psychologie notamment centrée sur
           l&apos;ergonomie et le handicap, je me suis orientée vers les métiers
-          du numérique. Étant à la recherche d&apos;kun métier où je peux
+          du numérique. Étant à la recherche d&apos;un métier où je peux
           exprimer ma créativité tout en gardant un contact humain, je me suis
           dirigée vers l&apos;UX Design. Aujourd&apos;hui je suis développeuse
           front-end, je suis capable de réaliser des sites webs en HTML, CSS,
@@ -18,6 +19,7 @@ function About() {
         </p>
       </div>
       <img className={styles.neon} src={flower} alt="" />
+      <img className={styles.neonMobile} src={flowerMobile} alt="" />
     </div>
   );
 }
